150518f776d30b21d9cb395c58af8088cd8ed33b
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2019-12-31 10:58:50 +0200

    first endpoint

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -4,4 +4,9 @@ import { TasksService } from './tasks.service';
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
 }
